3a38607a926bc118b7f6c529224d6902de9cc05e
Quillraven <herf@gmx.at>
2018-12-23 18:54:05 +0100

    static collision bodies are created out of tiledMap

--- core/src/com/quillraven/masamune/map/MapManager.kt
+++ core/src/com/quillraven/masamune/map/MapManager.kt
@@ -1,9 +1,13 @@
 package com.quillraven.masamune.map
 
 import com.badlogic.gdx.Gdx
+import com.badlogic.gdx.maps.objects.PolygonMapObject
+import com.badlogic.gdx.maps.objects.PolylineMapObject
+import com.badlogic.gdx.maps.objects.RectangleMapObject
 import com.badlogic.gdx.maps.tiled.TiledMap
-import com.quillraven.masamune.MainGame
-import com.quillraven.masamune.UNIT_SCALE
+import com.badlogic.gdx.physics.box2d.BodyDef
+import com.badlogic.gdx.physics.box2d.ChainShape
+import com.quillraven.masamune.*
 
 private const val TAG = "MapManager"
 
@@ -11,18 +15,85 @@ class MapManager constructor(game: MainGame) {
     private val ecsEngine = game.ecsEngine
     private val assetManger = game.assetManager
     private val gameEventManager = game.gameEventManager
+    private val world = game.world
+    private val rectVertices = FloatArray(8)
 
     fun setMap(type: EMapType) {
         val tiledMap = assetManger.get(type.filePath, TiledMap::class.java)
 
+        loadCollisionObjects(tiledMap)
         loadCharacters(tiledMap)
 
         gameEventManager.mapEvent.newTiledMap = tiledMap
         gameEventManager.mapSignal.dispatch(gameEventManager.mapEvent)
     }
 
+    private fun loadCollisionObjects(tiledMap: TiledMap) {
+        val mapLayer = tiledMap.layers.get("collision")
+        if (mapLayer == null) {
+            Gdx.app.debug(TAG, "There is no collision layer")
+            return
+        }
+
+        for (mapObj in mapLayer.objects) {
+            when (mapObj) {
+                is RectangleMapObject -> {
+                    val rect = mapObj.rectangle
+                    // bot-left
+                    rectVertices[0] = 0f
+                    rectVertices[1] = 0f
+                    // top-left
+                    rectVertices[2] = 0f
+                    rectVertices[3] = rect.height
+                    // top-right
+                    rectVertices[4] = rect.width
+                    rectVertices[5] = rect.height
+                    // bot-right
+                    rectVertices[6] = rect.width
+                    rectVertices[7] = 0f
+                    createCollisionObject(rect.x, rect.y, rectVertices, true)
+                }
+                is PolylineMapObject -> createCollisionObject(mapObj.polyline.x, mapObj.polyline.y, mapObj.polyline.vertices)
+                is PolygonMapObject -> createCollisionObject(mapObj.polygon.x, mapObj.polygon.y, mapObj.polygon.vertices, true)
+                else -> Gdx.app.debug(TAG, "Unsupported collision map object of type ${mapObj.javaClass}")
+            }
+        }
+    }
+
+    private fun createCollisionObject(x: Float, y: Float, vertices: FloatArray, loop: Boolean = false) {
+        resetBodyAndFixtureDef()
+        bodyDef.type = BodyDef.BodyType.StaticBody
+        bodyDef.position.set(x * UNIT_SCALE, y * UNIT_SCALE)
+        bodyDef.fixedRotation = true
+        val body = world.createBody(bodyDef)
+
+        for (i in vertices.indices) {
+            vertices[i] *= UNIT_SCALE
+        }
+
+        val chainShape = ChainShape()
+        if (loop) {
+            chainShape.createLoop(vertices)
+        } else {
+            chainShape.createChain(vertices)
+        }
+        fixtureDef.shape = chainShape
+        fixtureDef.isSensor = false
+        body.createFixture(fixtureDef)
+        chainShape.dispose()
+
+        for (i in vertices.indices) {
+            vertices[i] /= UNIT_SCALE
+        }
+    }
+
     private fun loadCharacters(tiledMap: TiledMap) {
         val mapLayer = tiledMap.layers.get("characters")
+        if (mapLayer == null) {
+            Gdx.app.debug(TAG, "There is no characters layer")
+            return
+        }
+
         for (mapObj in mapLayer.objects) {
             val charType = mapObj.properties.get("type", "", String::class.java)
             if (charType.isBlank()) {
